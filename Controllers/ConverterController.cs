673108a7d41f9330af231eeef9ba8ee81815b92d
bmalz <bartek@bmalz.pro>
2020-11-21 21:43:47 +0100

    Implement ConverterController

---
+++ Controllers/ConverterController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HtmlToOpenXml;
+using System.IO;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace Service.HTML2DOCXConverter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConverterController : ControllerBase
+    {
+        private readonly ILogger<ConverterController> _logger;
+
+        public ConverterController(ILogger<ConverterController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            string body = string.Empty;
+            using (var reader = new System.IO.StreamReader(Request.Body))
+            {
+                body = await reader.ReadToEndAsync();
+            }
+            
+            if(string.IsNullOrWhiteSpace(body))
+                return BadRequest();
+
+            using (MemoryStream generatedDocument = new MemoryStream())
+     {
+          using (WordprocessingDocument package = WordprocessingDocument.Create(generatedDocument, WordprocessingDocumentType.Document))
+          {
+               MainDocumentPart mainPart = package.MainDocumentPart;
+               if (mainPart == null)
+               {
+                    mainPart = package.AddMainDocumentPart();
+                    new Document(new Body()).Save(mainPart);
+               }
+
+               HtmlConverter converter = new HtmlConverter(mainPart);
+               converter.ParseHtml(body);
+
+               mainPart.Document.Save();
+          }
+
+          return new FileStreamResult(generatedDocument, "application/octet-stream");
+     }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using HtmlToOpenXml;
 
 namespace Service.HTML2DOCXConverter
 {
